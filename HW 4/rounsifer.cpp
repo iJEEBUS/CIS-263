82c32bf7ac0e24148cb356ebd3d9a7f42b152ba5
Ronald Rounsifer <ronrounsifer@me.com>
2018-02-23 23:14:10 -0500

    rm using namespace std

--- HW 4/rounsifer.cpp
+++ HW 4/rounsifer.cpp
@@ -3,19 +3,17 @@
 #include <map>
 #include <regex>
 
-using namespace std;
-
 int main() {
-    map <string, int> word_map; // empty map container
-    regex r("^[^a-zA-Z]+$"); // a string has to consist of letters
-    regex a("(a)");
-    regex d("(the)");
-    smatch m; // regex matches
+    std::map <std::string, int> word_map; // empty map container
+    std::regex r("^[^a-zA-Z]+$"); // a string has to consist of letters
+    std::regex a("(a)");
+    std::regex d("(the)");
+    std::smatch m; // regex matches
 
-    ifstream inFile;
-    ofstream outFile;
-    string word;
-    string last_char;
+    std::ifstream inFile;
+    std::ofstream outFile;
+    std::string word;
+    std::string last_char;
     inFile.open("input.txt");
     outFile.open("output.txt");
     while (inFile >> word)
@@ -30,7 +28,7 @@ int main() {
 	// If detected, check for an existing string of the same word minus the period.
 	// If not detected, check for the string.
         if (last_char.compare(".") == 0 && word_map.find(word.substr(0,word.size()-1)) == word_map.end()) {
-            word_map.insert(map<string, int>::value_type(word.substr(0,word.size()-1), 1));
+            word_map.insert(std::map<std::string, int>::value_type(word.substr(0,word.size()-1), 1));
         }
         else if (last_char.compare(".") && word_map.find(word.substr(0,word.size()-1)) != word_map.end()) {
             word_map.find(word)->second++;
@@ -39,7 +37,7 @@ int main() {
             word_map.find(word)->second++;
         }
         else if (word_map.find(word) == word_map.end()) {
-            word_map.insert(map<string, int>::value_type(word, 1));
+            word_map.insert(std::map<std::string, int>::value_type(word, 1));
         }
     }
 
@@ -47,7 +45,7 @@ int main() {
     for(auto it = word_map.begin(); it != word_map.end(); it++)
     {
 	// FIX THIS
-	// Figure out how to combine all of the regexes into one
+	// Figure out how to combine all three regex values into one
         if (regex_match(it->first, m, r) | regex_match(it->first, m, a) | regex_match(it->first, m, d))
             word_map[it->first] = 0;
 
