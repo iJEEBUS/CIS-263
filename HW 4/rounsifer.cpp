f4dd818f3fbd014f5a11fb7d417180656db49410
Ronald Rounsifer <ronrounsifer@me.com>
2018-02-24 03:20:07 -0500

    complete

--- HW 4/rounsifer.cpp
+++ HW 4/rounsifer.cpp
@@ -4,48 +4,43 @@
 #include <regex>
 
 int main() {
-    std::map <std::string, int> word_map; // empty map container
-    std::regex r("(^[^a-zA-Z]+$|(a)|(the))"); // a string has to consist of letters
+    const char *last_char; // used for punctuation testing
+    std::map<std::string, int> word_map; // empty map container
+    std::regex r("(^[^a-zA-Z]+$|(a)|(the))"); // regex = non-letters | a | the
     std::smatch m; // regex matches
-
     std::ifstream inFile;
     std::ofstream outFile;
-    std::string word, last_char;
+    std::string word;
     inFile.open("input.txt");
     outFile.open("output.txt");
-    while (inFile >> word)
-    {
-        // convert to lowercase
+
+    while (inFile >> word) {
+        last_char = word.substr(word.size() - 1, word.size() - 1).c_str();
+
+        // convert word to lowercase
         transform(word.begin(), word.end(), word.begin(), ::tolower);
-        
-	    // last character of "word" - used to check for periods
-	    last_char = word.substr(word.size() - 1, word.size()-1);
-        
-	    // Checks for period in strings
-	    // If detected, check for an existing string of the same word minus the period.
-	    // If not detected, check for the string.
-        if (last_char.compare(".") == 0 && word_map.find(word.substr(0,word.size()-1)) == word_map.end()) {
-            word_map.insert(std::map<std::string, int>::value_type(word.substr(0,word.size()-1), 1));
-        }
-        else if (last_char.compare(".") && word_map.find(word.substr(0,word.size()-1)) != word_map.end()) {
+
+        // Checks for punctuation at the end of words to make sure we do
+        // not have duplicates (Ex...files - files. || tests - tests?)
+        if (ispunct((int) *last_char) != 0) {
+            if (word_map.find(word.substr(0, word.size() - 1)) == word_map.end())
+                word_map.insert(std::map<std::string, int>::value_type(word.substr(0, word.size() - 1), 1));
+            if (word_map.find(word.substr(0, word.size() - 1)) != word_map.end())
+                word_map.find(word)->second++;
+        } else if (word_map.find(word) != word_map.end()) {
             word_map.find(word)->second++;
-        }
-        else if (word_map.find(word) != word_map.end()){
-            word_map.find(word)->second++;
-        }
-        else if (word_map.find(word) == word_map.end()) {
+        } else if (word_map.find(word) == word_map.end()) {
             word_map.insert(std::map<std::string, int>::value_type(word, 1));
         }
     }
 
     // Sets value to 0 if not a word and writes counted words to file
-    for(auto it = word_map.begin(); it != word_map.end(); it++)
-    {
+    for (auto it = word_map.begin(); it != word_map.end(); it++) {
         if (regex_match(it->first, m, r))
             word_map[it->first] = 0;
 
         if (it->second > 0)
-		outFile << it->first << ' ' << it->second << "\n";
+            outFile << it->first << ' ' << it->second << "\n";
     }
     inFile.close();
     outFile.close();
